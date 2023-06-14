610cc0aa664df09898edfd8be3f75d0d527e49e0
zzx <zzxjava@163.com>
2023-06-14 10:32:03 +0800

    redissonClient

--- parent/framework/mp-boot-starter/src/main/java/com/thy/backend/parent/framework/mp/SnowflakeRedissonConfig.java
+++ parent/framework/mp-boot-starter/src/main/java/com/thy/backend/parent/framework/mp/SnowflakeRedissonConfig.java
@@ -47,7 +47,7 @@ public class SnowflakeRedissonConfig implements SmartLifecycle {
             @Value("snowflake.redisson.mode") String mode,
             ApplicationContext context
     ) throws Exception {
-        log.info("mode: {}", mode)
+        log.info("mode: {}", mode);
         RedissonClient client = getRedissonClient(mode, context);
 
         Random random = new Random();
